07b508c426f627a7bedb86102ba52c333f537604
Tom <tomlevannier@yahoo.com>
2025-01-29 16:22:33 +0100

    ouioui

--- Assets/Scripts/SeaweedsSystem/SeaweedManager.cs
+++ Assets/Scripts/SeaweedsSystem/SeaweedManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using BubbleIdle.Core.SeaweedsSystem;
 using BubbleIdle.SaveSystem;
 using BubbleIdle.SeaweedSystem;
 using LTX.Singletons;
@@ -15,13 +16,16 @@ namespace BubbleIdle.Core
         protected override void Awake()
         {
             base.Awake();
+            int secondsPassed = Mathf.FloorToInt(GameController.ProgressionManager.SecondsPassed);
             Debug.Log($"Time since last login : {Mathf.FloorToInt(GameController.ProgressionManager.SecondsPassed)} seconds");
+            CalculateOfflineBubbleProduction();
 
             foreach (SeaweedSave seaweedSave in GameController.ProgressionManager.seaweeds)
             {
                 Seaweed newSeaweed = Instantiate(seaweedPrefab, seaweedSave.seaweedPosition, Quaternion.identity);
                 seaweeds.Add(newSeaweed);
                 newSeaweed.Initialize(seaweedSave.seaweedData, seaweedSave.seaweedLevel);
+                
             }
             //GameController.ProgressionManager.SecondsPassed
         }
@@ -33,12 +37,23 @@ namespace BubbleIdle.Core
                 seaweed.Refresh();
             }
         }
-
         public void AddSeaweed(Seaweed newSeaweed)
         {
             seaweeds.Add(newSeaweed);
             newSeaweed.transform.position = seaweedsPos[newSeaweed.data.seaweedType].position;
             GameController.ProgressionManager.AddSeaweed(newSeaweed);
+        }
+
+        private void CalculateOfflineBubbleProduction()
+        {
+            foreach (SeaweedSave seaweedSave in GameController.ProgressionManager.seaweeds)
+            {
+                float bubbleProductionRate;
+                bubbleProductionRate = seaweedSave.seaweedData.bubbleProductionRate;
+                int bubblesProduced = Mathf.FloorToInt(bubbleProductionRate * GameController.ProgressionManager.SecondsPassed);
+                ResourcesManager.Instance.AddBubbles(bubblesProduced);
+                Debug.Log($"Seaweed {seaweedSave.seaweedData.seaweedType} produced {bubblesProduced} bubbles while offline.");
+            }
         }
     }
 }
