b8e502a7234596e8a08ca75acff09989d8e19008
Luanqpa <rastello.boulot@gmail.com>
2025-01-29 12:37:21 +0100

    ui plus mieu que moin bien

--- Assets/Scripts/UI/PSS.cs
+++ Assets/Scripts/UI/PSS.cs
@@ -1,28 +1,22 @@
-using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PSS : MonoBehaviour
 {
-    [SerializeField] private Transform panel;
-    [SerializeField] private float distance; //400 & 1080
-    [SerializeField] private bool isOn;
     [SerializeField] private Button button; // Reference to the button
     [SerializeField] private Sprite activeIcon; // Icon when active
     [SerializeField] private Sprite inactiveIcon; // Icon when inactive
+    [SerializeField] private bool isOn;
 
-    //when Button is 1(on), move Button and Upgrade on x axis by x
     public void ClickButton1()
     {
         if (!isOn)
         {
-            panel.DOMoveX(panel.position.x - distance, 0.6f);
             button.image.sprite = activeIcon; // Change icon to active
             isOn = true;
         }
         else
         {
-            panel.DOMoveX(panel.position.x + distance, 0.6f);
             button.image.sprite = inactiveIcon; // Change icon to inactive
             isOn = false;
         }
--- Assets/Scripts/UI/PanelSlide.cs
+++ Assets/Scripts/UI/PanelSlide.cs
@@ -5,7 +5,7 @@ public class PanelSlide : MonoBehaviour
 {
     [SerializeField] private Transform panel;
     [SerializeField] private float distance; //400 & 1080
-    [SerializeField] private bool isOn;
+    [SerializeField] public bool isOn;
     
     //when Button is 1(on), move Button and Upgrade on x axis by x
     public void ClickButton()
