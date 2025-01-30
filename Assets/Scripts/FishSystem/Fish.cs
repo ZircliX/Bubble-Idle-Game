e854ed3c63413cb1bfaa5a6a5a07f9cdced64e65
Tom <tomlevannier@yahoo.com>
2025-01-30 18:20:37 +0100

    fishy

--- Assets/Scripts/FishSystem/Fish.cs
+++ Assets/Scripts/FishSystem/Fish.cs
@@ -18,10 +18,14 @@ namespace BubbleIdle.FishSystem
 
         public void Refresh()
         {
+            Vector3 direction = targetPosition - transform.position;
+            if (direction != Vector3.zero)
+            {
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(0, 0, angle);
+            }
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, data.speed * Time.deltaTime);
-            
-            
-            if (Vector3.Distance(transform.position, targetPosition) <=1)
+            if (Vector3.Distance(transform.position, targetPosition) <= 1)
             {
                 SetNewTargetPosition();
             }
