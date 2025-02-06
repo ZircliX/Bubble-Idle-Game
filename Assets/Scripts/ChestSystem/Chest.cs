ded36ed772c40a128cc5a5e154306b0c7c5d8ceb
ttakkeee <157479951+ttakkeee@users.noreply.github.com>
2025-02-06 12:11:53 +0100

    Chest
    Chest pop up, panels, superbubbles purchase..

---
+++ Assets/Scripts/ChestSystem/Chest.cs
@@ -0,0 +1,67 @@
+using Codice.CM.Common.Tree.Partial;
+using DG.Tweening;
+using UnityEngine;
+
+namespace BubbleIdle.Chest
+{
+    public class Chest : MonoBehaviour
+    {
+        public GameObject popUp;
+        [SerializeField] private GameObject purchasePanel;
+        [SerializeField] private GameObject notEnoughPanel;
+        [SerializeField] private Transform spawnPoint;
+        [SerializeField] private Animator chestAnimator;
+        private bool isChestOpened = false;
+
+        public void Update()
+        {
+            if (GameController.ResourcesManager.SpecialBubbleCount >= 10) 
+            {
+                popUp.SetActive(true);
+            }
+            else
+            {
+                popUp.SetActive(false);
+            }
+        }
+
+        public void OnChestClick()
+        {
+            if (isChestOpened)
+            {
+                return;
+            }
+
+            if (GameController.ResourcesManager.SpecialBubbleCount >= 10)
+            {
+                GameController.ResourcesManager.SpendSpecialBubbles(10);
+                purchasePanel.SetActive(true);
+
+                if (chestAnimator != null)
+                {
+                    chestAnimator.enabled = true;
+                    chestAnimator.Play("ChestOpen");
+                }
+
+                DisableChest();
+            }
+            else
+            {
+                GameObject info = Instantiate(notEnoughPanel, spawnPoint);
+                info.transform.DOLocalMoveY(info.transform.localPosition.y + 50, 2f);
+                Destroy(info, 2f);
+            }
+        }
+
+        private void DisableChest()
+        {
+            isChestOpened = true;
+            
+            //Collider2D collider = GetComponent<Collider2D>();
+            //if (collider != null)
+            {
+                //collider.enabled = false;
+            }
+        }
+    }
+}
